9aaccf2dc4e868755f60be0d4bb75e968820154a
Cicedunet <Bouonoucicero@gmail.com>
2025-03-05 21:37:15 +0100

    Initial commit with blog project

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list, name='post_list'),
+    path('', views.index, name='index'),
        path('post', views.list, name='post_list'),
     
     path('post/<int:id>/', views.detail, name='post_detail'),
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
+from .forms import CommentForm
 from django.core.paginator import Paginator
 
 # Create your views here.
@@ -8,14 +9,34 @@ def index(request):
 
 def list(request):
     post_list = Post.objects.all()
-    paginator = Paginator(post_list, 1)  # 5 posts per page
-
+    paginator = Paginator(post_list, 2)  # 5 posts per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
+   # paginator = Paginator(post_list, 2)  # 5 posts per page
+
+   # page_number = request.GET.get('page')
+   # page_obj = paginator.get_page(page_number)
     
     return render(request, 'blog/post_list.html', {'page_obj': page_obj})
 
 def detail(request, id):
-    post = Post.objects.get(id=id)
-    return render(request, "blog/post_detail.html", {"post_detail": post})
+    post = get_object_or_404(Post, id=id)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, "blog/post_detail.html", {
+        "post_detail": post,
+        "comments": comments,
+        "new_comment": new_comment,
+        "comment_form": comment_form
+    })
 
